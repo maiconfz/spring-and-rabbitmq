e9622e8eeab27426e2d3cdc7f00a799ff6031890
Maiconfz <maiconfz@gmail.com>
2024-08-04 14:45:20 +0100

    Init project with spring initializr with a updated gradle wrapper

---
+++ src/test/java/io/github/maiconfz/spring_and_rabbitmq/SpringAndRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.spring_and_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
